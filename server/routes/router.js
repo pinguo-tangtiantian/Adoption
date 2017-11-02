f4b79e43fac80ee92363672c7dfc754a89992313
pinguo-tangtiantian <tangtiantian@camera360.com>
2017-11-02 19:30:59 +0800

    新增处理上传动物信息处理路由

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,16 +3,21 @@ var express = require('express'),
 var path = require('path');
 var SQL = require('../conf/mysql');
 
-router.get('/', function(req, res) {
-    res.render('home.html');
-});
+// router.get('/', function(req, res) {
+//     res.render('home.html');
+// });
 
-router.get('/guide', function(req, res) {
-    res.render('guide.html');
-});
+// router.get('/guide', function(req, res) {
+//     res.render('guide.html');
+// });
 
 router.get('/upload', function(req, res) {
     res.render('upload.html');
 });
+
+router.post('/upload_form', function(req, res) {
+    res.send("666666");
+});
+
 
 module.exports = router;
