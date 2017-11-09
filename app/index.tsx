95345cbe2d4c98aeefcf5d77a40eb2f2f464d986
pinguo-tangtiantian <tangtiantian@camera360.com>
2017-11-09 18:20:41 +0800

    程序入口文件

---
+++ app/index.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+import myCreateStores from './store';
+
+import Navigator from './components/common/top_nav';
+import Footer from './components/common/footer';
+import { RootApp } from './components/common/root_app';
+import { HomePage } from './components/pages/home';
+import { UploadPage } from './components/pages/upload';
+// import { GuidePage } from './components/pages/guide';
+import { SignPage } from './components/pages/login';
+
+import './static/css/style.css';
+
+const store: any = myCreateStores();
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <div>
+                <Navigator />
+                <Route exact path="/" component={HomePage} />
+                <Route path="/upload" component={UploadPage} />
+                <Route path="/login" component={SignPage} />
+                <Footer />
+            </div>
+        </Router>
+    </Provider>
+    , document.getElementById("app")
+)
