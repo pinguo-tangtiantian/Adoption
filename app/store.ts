509f99f4f174c64e328ab11ecdfcb531e9cd566d
pinguo-tangtiantian <tangtiantian@camera360.com>
2017-11-09 18:21:19 +0800

    生成store

---
+++ app/store.ts
@@ -0,0 +1,16 @@
+import { applyMiddleware, combineReducers, createStore, StoreCreator, StoreEnhancer } from 'redux';
+import thunk from 'redux-thunk';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+import * as reducers from './reducer/index';
+
+export default function myCreateStores(){
+    const thunkMiddleWare: any = applyMiddleware(thunk);
+    const store: any = createStore(
+        combineReducers({
+            ...reducers,
+            routing: routerReducer
+        }), thunkMiddleWare
+    );
+    // const store = createStore(combineReducers({...reducers}), thunkMiddleWare);
+    return store;
+}
