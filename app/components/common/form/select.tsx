d9fe9f112d60501e4f1980c2bdeee50f409e8878
pinguo-tangtiantian <tangtiantian@camera360.com>
2017-11-09 18:22:47 +0800

    下拉选择组件

---
+++ app/components/common/form/select.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+interface SelectProps{
+    item: any
+};
+interface SelectState{};
+
+export default class Select extends React.Component<SelectProps, SelectState>{
+    constructor(props: SelectProps){
+        super(props);
+    }
+
+    render() {
+        const { item } = this.props;
+        return(
+            <div className="upload-item">
+                <label>{item.item_name}:</label>
+                <select name={item.name} value={item.options[0].value}>
+                    {
+                        item.options.map(option =>{
+                            return(
+                                <option key={option} value={option}>{option}</option>
+                            )
+                        })
+                    }
+                </select>
+            </div>
+        )
+    }
+}
