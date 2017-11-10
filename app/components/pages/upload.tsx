aa42be850f06c576c106b6973106cd3200e46d69
pinguo-tangtiantian <tangtiantian@camera360.com>
2017-11-10 17:38:30 +0800

    实现上传功能

--- app/components/pages/upload.tsx
+++ app/components/pages/upload.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 import Input from '../common/form/input';
 import Select from '../common/form/select';
 import Check from '../common/form/check';
 
-interface IAppProps { };
-interface IAppState { };
+import * as actions from '../../actions';
+
+interface IAppProps {
+    actions: any
+};
+interface IAppState {
+    photos: any[]
+};
 
 const formOptions: any[] = [
     {
@@ -57,16 +65,74 @@ const formOptions: any[] = [
         type: "input",
         item: { item_name: "备注", name: "remark", placeholder: "最多500字" }
     },
+    {
+        type: "image",
+        item: { item_name: "照片", name: "photos", max: 6, min: 1 }
+    },
 ]
 
-export class UploadPage extends React.Component<IAppProps, IAppState>{
+class UploadPage extends React.Component<IAppProps, IAppState>{
     constructor(props: IAppProps) {
         super(props);
+        this.state = {
+            photos: []
+        }
     }
 
+    componentDidMount() {
+    }
+
+    onFormDataPost = (e) => {
+        e.preventDefault();
+        const dom: HTMLFormElement = document.getElementById("upload_form") as HTMLFormElement;
+        const form: FormData = new FormData(dom);
+        this.props.actions.fetchAnimalData(form);
+    }
+
+    onSelectPhoto = (e) => {
+        const input: HTMLInputElement = e.target;
+        const photos: any[] = this.state.photos;
+        if (!input.files || !input.files[0]) {
+            return;
+        }
+
+        if (input.files[0].size > (1024 * 1024 * 5)) {    //图片大于5m
+            return;
+        }
+        
+        const filename: string = input.files[0].name;
+        let duplicateFlag: boolean = false;
+        for(let i in photos){
+            if(photos[i].name == filename){
+                alert("重复啦");
+                duplicateFlag = true;
+                break;
+            }
+        }
+
+        if(duplicateFlag){ return };
+
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(input.files[0]);
+        fileReader.onload = (event) => {
+            const baseCode: string = event.target.result;
+            let photo: any = {
+                name: filename,
+                src: baseCode
+            }
+            let statePhotos = this.state.photos;
+            statePhotos.push(photo)
+            this.setState({
+                photos: statePhotos
+            })
+        }
+    }
+    
+
     render(): JSX.Element {
+        console.log(this.state.photos);
         return (
-            <form className="upload_form" id="upload_form" encType="multipart/form-data">
+            <form className="upload_form" id="upload_form" encType="multipart/form-data" name="uoload_animal">
                 {
                     formOptions.map((formItem, index) => {
                         if (formItem.type == "input") {
@@ -81,11 +147,48 @@ export class UploadPage extends React.Component<IAppProps, IAppState>{
                             return (
                                 <Check key={formItem.item.item_name} item={formItem.item} />
                             )
+                        } else if (formItem.type == "image") {
+                            return (
+                                <div className="">
+                                    <label>高清美照</label>
+                                    <div className="photos">
+                                        <div className="btn-div">
+                                            <p className="upload-tips">每次只能选择一张，最多上传6张</p>
+                                            <div className="btn-box">
+                                                <span className="upload-img-btn">上传</span>
+                                                <input type="file" name="photos" id="photos" className="upload-photo" onChange={this.onSelectPhoto} onClick={(event)=> {event.target.value = null}} />
+                                            </div>
+                                        </div>
+                                        <ul className="photos-ul">
+                                            {
+                                                this.state.photos.map((photo, index) => {
+                                                    return (
+                                                        <li key={`photo${index}`}>
+                                                            <img className="photo_thumb" src={photo.src} />
+                                                        </li>
+                                                    )
+                                                })
+                                            }
+                                        </ul>
+                                    </div>
+                                </div>
+                            )
                         }
                     })
                 }
-                <input type="submit" value="提交" />
+                <input type="button" value="提交" className="btn" onClick={this.onFormDataPost} />
             </form>
         )
     }
 }
+
+function mapDispatchToProps(dispatch: any) {
+    return {
+        actions: bindActionCreators(actions, dispatch)
+    }
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(UploadPage);
