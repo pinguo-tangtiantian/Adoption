4d8e59f0c6012fe6ddb8e6fc88107c1cb9a66041
pinguo-tangtiantian <tangtiantian@camera360.com>
2017-11-16 18:33:07 +0800

    修改登录页面

--- app/components/pages/login.tsx
+++ app/components/pages/login.tsx
@@ -1,18 +1,48 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
-import { SignUp, SignIn } from '../common/sign_form';
+import { SignUp, SignIn, FindPwd } from '../common/sign_form';
 
-interface SignProps{};
+interface SignProps{
+    signDisplay: any,
+    actions: any
+};
 interface SignState{};
 
-export class SignPage extends React.Component<SignProps, SignState>{
+export default class SignPage extends React.Component<SignProps, SignState>{
+    constructor(props: SignProps){
+        super(props);
+    }
+
+
+
     render(): JSX.Element {
         return (
             <div className="content">
-                <SignUp />
                 <SignIn />
+                <SignUp />
+                <FindPwd />
             </div>
         )
     }
 }
+/* function mapStateToProps(state: any) {
+    return {
+        signDisplay: state.reducers.changeHeaderState
+    }
+}
+
+function mapDispatchToProps(dispatch: any) {
+    return {
+        actions: bindActionCreators(actions, dispatch)
+    }
+}
+
+console.log(typeof(mapStateToProps))
+export default connect({
+    mapStateToProps,
+    mapDispatchToProps
+})(SignPage); */
