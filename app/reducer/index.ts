46606fe38373a2537512288203dda7f133044b60
pinguo-tangtiantian <tangtiantian@camera360.com>
2017-11-16 18:34:05 +0800

    新增切换登录框的reducer

--- app/reducer/index.ts
+++ app/reducer/index.ts
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 
 import changeHeaderState from './header';
 import uploadAnimal from './upload_animal';
+import changeSignDisplay from './sign';
 
 
 export const reducers: any = combineReducers({
     "changeHeaderState": changeHeaderState,
-    "uploadAnimal": uploadAnimal
+    "uploadAnimal": uploadAnimal,
+    "changeSignDisplay": changeSignDisplay
 })
