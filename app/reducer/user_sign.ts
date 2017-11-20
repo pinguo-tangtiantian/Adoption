8dd76c7b7f5cf83448ea0e839ffd675783a65716
pinguo-tangtiantian <tangtiantian@camera360.com>
2017-11-20 17:12:08 +0800

    忽略照片

--- app/reducer/user_sign.ts
+++ app/reducer/user_sign.ts
@@ -5,14 +5,14 @@ export default function userSign(state: any={
     pending: false,
     success: false}, action: AppAction){
     switch(action.type){
-        case "POST_SIGN_UP_DATA": { //注册中
+        case "SIGN_UP_PENDING": { //注册中
             return Object.assign({}, state, { 
                 signType: "sign_up",
                 pending: true,
                 success: false
              });
         }
-        case "RECEIVE_SIGN_UP_SUCCESS": { //注册成功
+        case "SIGN_UP_SUCCESS": { //注册成功
             return Object.assign({}, state, { 
                 signType: "sign_up",
                 pending: false,
@@ -20,7 +20,7 @@ export default function userSign(state: any={
                 data: action.data
              });
         }
-        case "RECEIVE_SIGN_UP_ERROR": { //注册失败
+        case "SIGN_UP_ERROR": { //注册失败
             return Object.assign({}, state, { 
                 signType: "sign_up",
                 pending: false,
@@ -28,14 +28,14 @@ export default function userSign(state: any={
                 data: action.data
              });
         }
-        case "POST_SIGN_IN_DATA": { //登录中
+        case "SIGN_IN_PENDING": { //登录中
             return Object.assign({}, state, { 
                 signType: "sign_in",
                 pending: true,
                 success: false
              });
         }
-        case "RECEIVE_SIGN_IN_SUCCESS": { //登录成功
+        case "SIGN_IN_SUCCESS": { //登录成功
             return Object.assign({}, state, { 
                 signType: "sign_om",
                 pending: false,
@@ -43,7 +43,7 @@ export default function userSign(state: any={
                 data: action.data
              });
         }
-        case "RECEIVE_SIGN_IN_ERROR": { //登录失败
+        case "SIGN_IN_ERROR": { //登录失败
             return Object.assign({}, state, { 
                 signType: "sign_in",
                 pending: false,
@@ -51,14 +51,14 @@ export default function userSign(state: any={
                 data: action.data
              });
         }
-        case "SEND_MODIFY_APPLY": { //发送修改请求中
+        case "PWD_APPLY_PENDING": { //发送修改请求中
             return Object.assign({}, state, { 
                 signType: "find_pwd",
                 pending: true,
                 success: false
              });
         }
-        case "RECEIVE_APPLY_SUCCESS": { //发送修改请求成功
+        case "PWD_APPLY_SUCCESS": { //发送修改请求成功
             return Object.assign({}, state, { 
                 signType: "find_pwd",
                 pending: false,
@@ -66,9 +66,31 @@ export default function userSign(state: any={
                 data: action.data
              });
         }
-        case "RECEIVE_APPLY_ERROR": { //发送修改请求失败
+        case "PWD_APPLY_ERROR": { //发送修改请求失败
             return Object.assign({}, state, { 
                 signType: "find_pwd",
+                pending: false,
+                success: false,
+                data: action.data
+             });
+        }case "PWD_MODIFY_PENDING": { //修改密码请求中
+            return Object.assign({}, state, { 
+                signType: "modify_pwd",
+                pending: true,
+                success: false
+             });
+        }
+        case "PWD_MODIFY_SUCCESS": { //修改密码成功
+            return Object.assign({}, state, { 
+                signType: "modify_pwd",
+                pending: false,
+                success: true,
+                data: action.data
+             });
+        }
+        case "PWD_MODIFY_ERROR": { //修改密码失败
+            return Object.assign({}, state, { 
+                signType: "modify_pwd",
                 pending: false,
                 success: false,
                 data: action.data
